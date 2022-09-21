f53167a1a60ab611367287aac9474c90781dde55
Thomas Duredon <thomas@ikuzo.fr>
2022-09-21 10:30:08 +0200

    add rma request state processor

---
+++ src/Processor/RMARequestProcessor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ikuzo\SyliusRMAPlugin\Processor;
+
+use Ikuzo\SyliusRMAPlugin\Entity\RMARequestInterface;
+
+class RMARequestProcessor
+{
+    public function __construct()
+    {
+    }
+
+    public function accept(RMARequestInterface $rmaRequest)
+    {
+    }
+
+    public function refuse(RMARequestInterface $rmaRequest)
+    {
+    }
+}
